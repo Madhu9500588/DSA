f807758a0d073c99f65d9ad54184df07655b3b1d
Madhumitha S <madhu.snpm2007@gmail.com>
2026-08-21 21:35:19 +0530

    Add InsertionSort class with insertion sort method
    Implement insertion sort algorithm for sorting an array.

---
+++ Arrays/GFG/InsertionSort.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void insertionSort(vector<int>& arr) {
+       for(int i=1 ; i<= arr.size()-1 ; i++)
+       {
+           int temp = arr[i];
+           int j = i-1;
+           while(j>=0 && arr[j]>temp)
+           {
+               arr[j+1] = arr[j];
+               j= j-1;
+               arr[j+1] = temp;
+           }
+       }
+        
+    }
+};
