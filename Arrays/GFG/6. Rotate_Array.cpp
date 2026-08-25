5a72267db5cd46164eea05f59d543cc762f68e36
Madhumitha S <madhu.snpm2007@gmail.com>
2026-08-25 18:50:18 +0530

    Add array rotation function in Rotate_Array.cpp
    Implemented a function to rotate an array by one position in clockwise direction.

---
+++ Arrays/GFG/6. Rotate_Array.cpp
@@ -0,0 +1,23 @@
+//Given an array arr, rotate the array by one position in clockwise direction.
+//Examples:
+
+//Input: arr[] = [1, 2, 3, 4, 5]
+//Output: [5, 1, 2, 3, 4]
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        
+       int n = arr.size();
+       int temp = arr[n-1];
+       {
+           for(int i =n-1; i>0; i--)
+           {
+               arr[i] = arr[i-1];
+           }
+           
+           arr[0] = temp;
+           
+       }
+    }
+};
